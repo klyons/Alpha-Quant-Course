ef1c41fa320f54122fa1270f19f4ed3fa9d5b08a
Kelly Franklin-Lyons <lyons.kelly@gmail.com>
2024-08-06 17:27:41 -0700

    added polygon support
    Signed-off-by: Kelly Franklin-Lyons <lyons.kelly@gmail.com>

--- Data/create_databases.py
+++ Data/create_databases.py
@@ -2,15 +2,16 @@ from datetime import datetime
 import pandas as pd
 import MetaTrader5 as mt5
 import requests
+from dotenv import load_dotenv
+import os
 import sys
 import pdb
 
-alphaVantageAPI = "G9ATZKK9US0QL6K8"
 
 mt5.initialize()
-
 print(mt5.account_info())
-
+load_dotenv()
+polygon_api_key = os.getenv('polygon_api_key')
 
 
 def get_save_path(symbol, timeframe):
@@ -70,46 +71,218 @@ def get_ticks(symbol, number_of_data=10_000, timeframe=mt5.TIMEFRAME_D1):
     df_ticks = df_ticks.set_index("time")
 
     return df_ticks
+import requests
+import pandas as pd
+import pdb
+import time
+from datetime import date, datetime, timedelta
 
-def get_alphaVantage(symbol, interval, key):
-    
+#https://polygon.io/docs/stocks/getting-started
 
-    # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-    r = requests.get(url)
-    data = r.json()
+polygon_api_key = "JlgRBshWYNKUeDMpSLOjvk7slj1GCpBh"  #JlgRBshWYNKUeDMpSLOjvk7slj1GCpBh
+polygon_rest_baseurl = "https://api.polygon.io/v2/"  #maybe trie v3as well
 
-    print(data)
+limit = 100000
 
-currency = "EURUSD!"
-timeframe = mt5.TIMEFRAME_M5
-# !! You can't import more than 99.999 rows in one request
-df = get_rates(currency, number_of_data=99_999, timeframe=timeframe) #AUDUSD-Z
-#df = get_rates("EURUSD!", number_of_data=99_999, timeframe=mt5.TIMEFRAME_H1)
-#df = get_rates("AUDUSD!", number_of_data=99_999, timeframe=mt5.TIMEFRAME_H1)
-#df = get_rates("USDJPY", number_of_data=99_999, timeframe=mt5.TIMEFRAME_H4)
+def get_tickers():
+    request_url = "https://api.polygon.io/v3/reference/tickers?active=true&limit=1000&apiKey={0}".format(polygon_api_key)
+    data = requests.get(request_url).json()
+    if data['status'] == 'OK':
+        df = pd.DataFrame(data['results'])
+        next_d = data['next_url']
+        while(next_d != {}):
+            next_d = "%s&apiKey=%s"%(next_d, polygon_api_key)
+            data = requests.get(next_d).json()
+            if data['status'] == 'OK':
+                tdf = pd.DataFrame(data['results'])
+                df = pd.concat([df, tdf])
+            if "next_url" in data.keys():
+                next_d = data['next_url']
+            else:
+                break
+        df.to_csv("tickers.csv")
+        print("Dumped all tickers to tickers.csv")
+    else:
+        print("No tickers downloaded")
 
-#put this as your input FixTimeBars/USDJPY_1H.csv
+def pull_data(symbol, start_time, multiplier, timespan):
+    """
+    date is a python date format
+    symbol is of the form XXXUSD
+    """
 
-# Display the data
-print(df)
+    # newest data at the bottom
+    sort = "asc"
 
-# Put where you want to save the database
-#sample
-#C:\ws\Alpha-Quant-Course\Data\FixTimeBars\USDCHF_M30_Forex.csv
+    if not start_time:
+        start_time = datetime.today() - timedelta(days=365*5) # polygon has a 5 year limitation
+        start_time =  datetime.date(start_time)
+    end_time = today = date.today()
 
-save_path = get_save_path(currency, timeframe)
-df.to_csv(save_path)
+    ## this means the symbol is an index and requires a differant API call
 
-# Save the database if you had put a path
-'''
-save_path = input("Write the path to store the file if you want to (if not, just press enter):")
+    request_url2 = f"{polygon_rest_baseurl}aggs/ticker/{symbol}/range/{multiplier}/" +\
+            f"{timespan}/{start_time}/{end_time}?adjusted=true&sort={sort}&" + \
+            f"limit={limit}&apiKey={polygon_api_key}"
 
-if len(save_path)>0:
+    if symbol[:2] == 'I:' or symbol[:2] == 'i:':  ## add string for index or symbol to allow for adjustment
+        request_url = "{0}aggs/ticker/{1}/range/{2}/{3}/{4}/{5}?adjusted=true&sort={6}&limit={7}&apiKey={8}".format(polygon_rest_baseurl, symbol, multiplier, timespan, start_time, end_time, sort, limit, polygon_api_key)
+
+    else:
+        request_url = "{0}aggs/ticker/{1}/range/{2}/{3}/{4}/{5}?adjusted=true&sort={6}&limit={7}&apiKey={8}".format(polygon_rest_baseurl, symbol, multiplier, timespan, start_time, end_time, sort, limit, polygon_api_key)
+
+
+    # If there is a connection error, the following try and catch should prevent Python from
+    # exiting. Instead, return no data and let the caller try again
+    try:
+        data = requests.get(request_url).json()
+    except:
+        print("Connection Error, try again")
+        return None 
+
+    if "results" in data:
+        return data["results"]
+    else:
+        return None
+
+
+def pull_dividend_data(symbol, start_time, multiplier, timespan):
+    """
+    date is a python date format
+    symbol is of the form XXXUSD
+    """
+
+    # newest data at the bottom
+    sort = "asc"
+
+    if not start_time:
+        start_time = datetime.today() - timedelta(days=365*5) # polygon has a 5 year limitation
+        start_time =  datetime.date(start_time)
+    end_time = today = date.today()
+
+    ## this means the symbol is an index and requires a differant API call
+    url = f'https://api.polygon.io/v3/reference/dividends/{symbol}?from={start_date}&to={end_date}&apiKey={polygon_api_key}'
+
+
+    request_url2 = f"{polygon_rest_baseurl}aggs/ticker/{symbol}/range/{multiplier}/" +\
+            f"{timespan}/{start_time}/{end_time}?adjusted=true&sort={sort}&" + \
+            f"limit={limit}&apiKey={polygon_api_key}"
+
+    if symbol[:2] == 'I:' or symbol[:2] == 'i:':  ## add string for index or symbol to allow for adjustment
+        request_url = "{0}aggs/ticker/{1}/range/{2}/{3}/{4}/{5}?adjusted=true&sort={6}&limit={7}&apiKey={8}".format(polygon_rest_baseurl, symbol, multiplier, timespan, start_time, end_time, sort, limit, polygon_api_key)
+
+    else:
+        request_url = "{0}aggs/ticker/{1}/range/{2}/{3}/{4}/{5}?adjusted=true&sort={6}&limit={7}&apiKey={8}".format(polygon_rest_baseurl, symbol, multiplier, timespan, start_time, end_time, sort, limit, polygon_api_key)
+
+
+    # If there is a connection error, the following try and catch should prevent Python from
+    # exiting. Instead, return no data and let the caller try again
+    try:
+        data = requests.get(request_url).json()
+    except:
+        print("Connection Error, try again")
+        return None 
+
+    if "results" in data:
+        return data["results"]
+    else:
+        return None
+
+
+def get_data(start_day, symbol, multiplier=1, timespan='hour', silent=False):
+    # start_day must be of datetime.date() format
+    # symbol must be uppercase
+    bars = []
+    mdf = pd.DataFrame()
+    interval = timespan
+    symbol = symbol.upper()
+    while(1):
+        bars = pull_data(symbol, start_day, multiplier, interval)
+        if not bars:
+            if not silent:
+                print("No data available for %s in this range %s "%(symbol, start_day))
+            break
+        df = pd.DataFrame(bars)
+        df["date_time"] = pd.to_datetime(df["t"], unit = "ms")
+        if ':' in symbol:
+            df = df[["date_time","o","h","c","l"]]
+            df.columns = ["date_time","open","high","low","close"]
+        if not ':' in symbol:    
+            df =  df[["date_time","o","h","c","l","v","vw"]]
+            df.columns = ["date_time","open","high","low","close","volume","vwap"]
+        df = df.sort_values("date_time")
+        if not silent and not df.empty:
+            print("Downloaded %s: %s - %s"%(symbol, df['date_time'].iloc[0], df['date_time'].iloc[-1]))
+
+        # When a symbol does not have anymore data, check for the start and end date here
+        # and if they are the same, then exit
+        if df['date_time'].iloc[0] == df['date_time'].iloc[-1]:
+            mdf = df
+            break
+
+        df.set_index(['date_time'], inplace=True)
+        # Make sure to get all of the data to this date
+        td = pd.to_datetime(date.today()) - df.index[-1]
+        if td.days > 1 or td.days == -1:
+            start_day = bars[-1]['t']
+            if interval == 'hour':
+                start_day = start_day + 60*60*1000*multiplier
+            elif interval == 'minute':
+                start_day = start_day + 60*1000*multiplier
+            elif interval == 'day':
+                start_day = start_day + 24*60*60*1000
+            else:
+                print("error, please pass in correct string for time granularity")
+            if mdf.empty:
+                mdf = df
+            else:
+                mdf = pd.concat([mdf, df], axis=0)
+        else:
+            if mdf.empty:
+                mdf = df
+                break
+            else:
+                mdf = pd.concat([mdf, df], axis=0)
+                break
+    #return mdf
+    save_path = get_save_path(symbol, interval)
+    mdf.to_csv(save_path)
+
+
+def get_currency():
+    currency = "EURUSD!"
+    timeframe = mt5.TIMEFRAME_M5
+    # !! You can't import more than 99.999 rows in one request
+    df = get_rates(currency, number_of_data=99_999, timeframe=timeframe) #AUDUSD-Z
+    #df = get_rates("EURUSD!", number_of_data=99_999, timeframe=mt5.TIMEFRAME_H1)
+    #df = get_rates("AUDUSD!", number_of_data=99_999, timeframe=mt5.TIMEFRAME_H1)
+    #df = get_rates("USDJPY", number_of_data=99_999, timeframe=mt5.TIMEFRAME_H4)
+
+    #put this as your input FixTimeBars/USDJPY_1H.csv
+
+    # Display the data
+    print(df)
+
+    # Put where you want to save the database
+    #sample
+    #C:\ws\Alpha-Quant-Course\Data\FixTimeBars\USDCHF_M30_Forex.csv
+    save_path = get_save_path(currency, timeframe)
     df.to_csv(save_path)
 
-'''
+    # Save the database if you had put a path
+    '''
+    save_path = input("Write the path to store the file if you want to (if not, just press enter):")
+
+    if len(save_path)>0:
+        df.to_csv(save_path)
+
+    '''
 
 
 ###### Exercise
 #- Do the same thing, for one of the 3 other function (copy_rates_range, copy_ticks_from or copy_ticks_range)
+
+
+if __name__ == '__main__':
+    #get_currency()
+    get_tickers()
