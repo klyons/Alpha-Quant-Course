41410dcaf6c405b818d7078f046e74950dd37642
TheRock <support@pandimit.com>
2025-02-13 07:47:07 -0800

    Add support for pacific time
    Signed-off-by: TheRock <support@pandimit.com>

--- libs/data_feed.py
+++ libs/data_feed.py
@@ -12,15 +12,34 @@ class DataFeed():
 		# create a connection to the broker using the APIs in
 		self.qt = quotes2.Quotes(self.broker, log=None)
 
-	def get_quote(self, symbol):
-		df = self.qt.get_broker_quote(symbol, lookback_days=10)
+	def get_quote(self, symbol, lookback_days=10):
+		df = self.qt.get_broker_quote(symbol, lookback_days=lookback_days, pacific_time=True, regular_trading_time=True)
 		if df.empty:
 			print("Unable to get data for %s"%symbol)
 		return df
+
+	def get_time_bars(self, df, frequency):
+		if df.empty:
+			print("Dataframe is empty, unable to get time bars")
+			return df
+		# convert the dataframe into time bars
+		# frequency: 3T, 5T, or 20T or 60T, etc
+		# Resample the DataFrame to frequency-minute intervals
+		rdf = df.resample(frequency).agg({
+		    'open': 'first',
+		    'high': 'max',
+		    'low': 'min',
+		    'close': 'last',
+		    'volume': 'sum'
+		})
+		rdf.dropna(inplace=True)
+		return rdf
 
 	def example_quote(self):
 		df = self.get_quote("spy")
 		print(df)
 if __name__ == '__main__':
 	data = DataFeed()
-	data.example_quote()
+	df = data.get_quote('spy', lookback_days=10)
+	rdf = data.get_time_bars(df, '60T')
+	print(rdf)
